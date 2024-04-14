a35807be8bc3f4d657737a7cec950922670b048e
Faiaz Hossain <faiazhossain4321@gmail.com>
2024-04-15 00:43:48 +0600

    updated

--- cyclone_form.php
+++ cyclone_form.php
@@ -14,7 +14,7 @@ if (isset($_POST['submit'])) {
     if ($result === TRUE) {
         echo '<div class="alert alert-success" role="alert">New cyclone record created successfully!</div>';
         echo "<script>console.log('New cyclone record created successfully!');</script>";
-        header("refresh:2; url=./view.php");
+        header("refresh:2; url=./cyclone_view.php");
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
